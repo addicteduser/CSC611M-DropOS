39c8134ab14b812a4c77adfa7edeb54f438d9b02
Darren <darren.sapalo@gmail.com>
2014-11-26 13:08:28 +0800

    Added a Config class that handles different IPs, ports, or paths.

---
+++ CSC611M Simple File Sharing/src/dropos/Config.java
@@ -0,0 +1,56 @@
+package dropos;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class Config {
+	private static Path path;
+	private static String ipAddress;
+	private static int port;
+	
+	public static void initialize(){
+		try {
+			File f = new File("config.ini");
+			Scanner s = new Scanner(f);
+			
+			while (s.hasNextLine()){
+				String currentLine = s.nextLine().trim().toUpperCase();
+				String[] pair = currentLine.split("=");
+				String var = pair[0].trim();
+				String value = pair[1].trim();
+				switch(var){
+				case "PATH": 
+					path = Paths.get(System.getProperty("user.dir")).resolve(value);
+				break;
+				case "IP":
+					ipAddress = value;
+					break;
+				case "PORT":
+					port = Integer.parseInt(value);
+					break;
+				default: 
+					System.err.println("Unknown variable [" + var + "] being assigned with " + value);
+					break;
+				}
+			}
+
+			s.close();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+	public static Path getPath() {
+		return path;
+	}
+
+	public static String getIpAddress() {
+		return ipAddress;
+	}
+
+	public static int getPort() {
+		return port;
+	}
+}
--- CSC611M Simple File Sharing/src/dropos/ServerDriver.java
+++ CSC611M Simple File Sharing/src/dropos/ServerDriver.java
@@ -2,16 +2,15 @@ package dropos;
 
 import java.io.IOException;
 
-public class Driver {
+public class ServerDriver {
 	
 	private static final int PORT_NUMBER = 4040;
 	private static final String HOSTNAME = "localhost";
 	
 	public static void main(String[] args) throws IOException {
+		Config.initialize();
+		
 		DropServer s = new DropServer(PORT_NUMBER);
 		s.start();
-		
-		DropClient c = new DropClient(HOSTNAME, PORT_NUMBER);
-		c.start();
 	}
 }
