82b6796b383da61e6bedcb092707f979ab360e61
Darren <darren.sapalo@gmail.com>
2014-11-26 13:10:41 +0800

    Port passed to server thread.

--- CSC611M Simple File Sharing/src/dropos/ServerDriver.java
+++ CSC611M Simple File Sharing/src/dropos/ServerDriver.java
@@ -4,13 +4,10 @@ import java.io.IOException;
 
 public class ServerDriver {
 	
-	private static final int PORT_NUMBER = 4040;
-	private static final String HOSTNAME = "localhost";
-	
 	public static void main(String[] args) throws IOException {
 		Config.initialize();
 		
-		DropServer s = new DropServer(PORT_NUMBER);
+		DropServer s = new DropServer(Config.getPort());
 		s.start();
 	}
 }
