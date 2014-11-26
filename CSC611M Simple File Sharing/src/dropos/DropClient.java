db96ed1376fead43da8e021c1b8b0266a3c039d5
addicteduser <tintin.kalaw@gmail.com>
2014-11-26 16:43:22 +0800

    CLIENT: can now send files!!!!!!

--- CSC611M Simple File Sharing/src/dropos/DropClient.java
+++ CSC611M Simple File Sharing/src/dropos/DropClient.java
@@ -1,9 +1,13 @@
 package dropos;
 
+import java.io.BufferedInputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.file.Path;
 
@@ -12,32 +16,55 @@ public class DropClient extends Thread {
 	private static String hostname;
 	private static Socket client;
 	private static DataInputStream in;
-	private static DataOutputStream out;
+	private static OutputStream out;
 	private Path clientPath;
-	
+
 	public DropClient() {
 		this.hostname = Config.getIpAddress();
 		this.port = Config.getPort();
 		this.clientPath = Config.getPath();
 		System.out.println("[SYSTEM] Created new CLIENT instance");
-		
+
 		try {
 			// create connection
 			System.out.println("[CLIENT] CLIENT trying to connect to SERVER at IP:" + hostname + " on port " + port);
 			client = new Socket(hostname, port);
 			System.out.println("[CLIENT] CLIENT just connected to SERVER. CLIENT has IP:" + client.getInetAddress().toString());
-			
+
 			// instantiate connections to get input from CLIENT
 			in = new DataInputStream(client.getInputStream());
 			// instantiate connections to get input from CLIENT
-			out = new DataOutputStream(client.getOutputStream());
+			out = client.getOutputStream();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void run() {
-		DirectoryWatcher.watchDirectoryPath(clientPath, out);
+		//DirectoryWatcher.watchDirectoryPath(clientPath, out);
+		try {
+			File myFile = clientPath.resolve("test.txt").toFile();
+			FileInputStream fis = new FileInputStream(myFile);
+			byte [] mybytearray  = new byte [(int)myFile.length()];
+			BufferedInputStream bin = new BufferedInputStream(fis);
+			bin.read(mybytearray, 0, mybytearray.length);
+			out.write(mybytearray, 0, mybytearray.length);
+			out.flush();
+			/*out.writeLong(myFile.length()); // send how large the file is
+			int n = 0;
+			while ((n = fis.read(mybytearray)) != -1) {
+				out.write(mybytearray, 0, n);
+				out.flush();
+			}*/
+			fis.close();
+			client.close();
+			System.out.println("Done.");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		while(true);
 	}
-	
+
 }
