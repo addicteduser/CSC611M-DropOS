153184128adb7caa49c5b8f1cd14c5504d1c485f
Darren <darren.sapalo@gmail.com>
2014-11-26 13:40:14 +0800

    Server now handles received message.

--- CSC611M Simple File Sharing/src/dropos/ClientConnection.java
+++ CSC611M Simple File Sharing/src/dropos/ClientConnection.java
@@ -1,7 +1,6 @@
 package dropos;
 
 import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.PrintWriter;
@@ -12,31 +11,63 @@ public class ClientConnection extends Thread {
 
 	private Socket connectionSocket;
 	private Scanner in;
-	private PrintWriter out;
+	public PrintWriter out;
+	private String ipAddress;
 
 	public ClientConnection(Socket connectionSocket) {
 		this.connectionSocket = connectionSocket;
+		this.ipAddress = connectionSocket.getInetAddress().toString()
+				.substring(1);
+
 		// TODO Auto-generated constructor stub
-		
-		System.out.println("[SYSTEM] SERVER just connected to CLIENT with IP:" + connectionSocket.getInetAddress().toString());
-		
+
+		System.out.println("[SYSTEM] SERVER just connected to CLIENT with IP:"
+				+ connectionSocket.getInetAddress().toString());
+
 		try {
-			DataInputStream din = new DataInputStream(connectionSocket.getInputStream());
+			DataInputStream din = new DataInputStream(
+					connectionSocket.getInputStream());
 			BufferedInputStream bin = new BufferedInputStream(din);
 			in = new Scanner(bin);
-			
-			DataOutputStream dout = new DataOutputStream(connectionSocket.getOutputStream());
+
+			DataOutputStream dout = new DataOutputStream(
+					connectionSocket.getOutputStream());
 			out = new PrintWriter(dout);
-	
-		}catch (Exception e){
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		super.run();
+		do {
+			String input = null;
+			// As long as you are sending me messages, handle them correctly
+			while ((input = in.nextLine()).equals(null) == false) {
+				handleInput(input);
+			}
+		} while (true);
+
 	}
 
+	private void handleInput(String input) {
+		String command = input.split(" ")[0];
+		String params = input.substring(command.length());
+
+		System.out.println("Command: " + command);
+		System.out.println("Params: " + params);
+		switch (command) {
+		case "ADD":
+			break;
+		case "MODIFY":
+			break;
+		case "DELETE":
+			break;
+		}
+	}
+
+	public String getIPAddress() {
+		return ipAddress;
+	}
 }
