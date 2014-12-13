6b1e205e6c87f2a55715640079bedbcda4f11851
Darren Karl A. Sapalo <darren.sapalo@gmail.com>
2014-12-13 08:01:38 +0800

    Added driver code for Coordinator.

--- CSC611M Simple File Sharing/src/drivers/CoordinatorDriver.java
+++ CSC611M Simple File Sharing/src/drivers/CoordinatorDriver.java
@@ -1,8 +1,16 @@
 package drivers;
 
+import java.io.IOException;
+
+import dropos.Config;
+import dropos.DropCoordinator;
+
 public class CoordinatorDriver {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		Config.initialize();
 		
+		DropCoordinator coordinator = new DropCoordinator(Config.getPort());
+		coordinator.run();
 	}
 }
