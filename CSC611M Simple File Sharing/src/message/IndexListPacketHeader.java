9e69548d7ddd00b2f0c0870ddbe2b86a21316392
addicteduser <tintin.kalaw@gmail.com>
2014-12-17 15:40:24 +0800

    [server] Added my own notes

--- CSC611M Simple File Sharing/src/message/IndexListPacketHeader.java
+++ CSC611M Simple File Sharing/src/message/IndexListPacketHeader.java
@@ -8,6 +8,7 @@ import java.nio.file.Files;
 import java.nio.file.LinkOption;
 import java.nio.file.attribute.BasicFileAttributes;
 
+import message.DropOSProtocol.HostType;
 import dropos.Config;
 
 public class IndexListPacketHeader extends FilePacketHeader {
