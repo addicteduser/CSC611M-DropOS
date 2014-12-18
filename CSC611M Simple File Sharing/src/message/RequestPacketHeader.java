f3f30a9988233d1df1c917fd09c1fe3b60e62405
addicteduser <tintin.kalaw@gmail.com>
2014-12-18 18:15:51 +0800

    Cleaned up RequestPacketHeader class.

--- CSC611M Simple File Sharing/src/message/RequestPacketHeader.java
+++ CSC611M Simple File Sharing/src/message/RequestPacketHeader.java
@@ -2,41 +2,21 @@ package message;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
-import javax.sound.midi.Receiver;
 
 import dropos.Config;
 
-public class RequestPacketHeader extends PacketHeader {
-
-	protected long filesize;
+public class RequestPacketHeader extends FilePacketHeader {
 	protected String filename;
-	protected File f;
 	
 	public RequestPacketHeader(String header) {
 		super(header);
-		String[] split = header.split(" ");
-		String command = split[0];
-		filename = split[1];
-		Path path = Config.getPath();
-		f = new File(path + "\\" + filename);
-		try {
-			filesize = Files.size(f.toPath());
-		} catch (IOException e1) {
-			System.err.println("Failed to parse filesize.");
-		}
-		
-		super.header = command + " " + filesize + " " + filename;
-		
-		System.out.println("HEADER: " + super.header);
+		filename = header.split(" ")[1];
 	}
 
 	@Override
 	public Message interpret(DropOSProtocol protocol) throws IOException {
-		protocol.sendFile(this, f);
-		return new FileAndMessage("REQUEST1 " + filename, f);
+		File f = new File(Config.getPath() + "\\" +  filename);
+		return new FileAndMessage("REQUEST " + filename, f);
 	}
 	
 	protected File receiveFile(DropOSProtocol protocol) throws IOException {
@@ -46,31 +26,4 @@ public class RequestPacketHeader extends PacketHeader {
 	protected String filePath() {
 		return Config.getPath() + "\\" + filename;
 	}
-	
-	public Message interpret2(DropOSProtocol protocol) throws IOException {
-		File file = receiveFile(protocol);
-		System.out.println("Requested file received");
-		return new FileAndMessage("REQUEST " + filename, file);
-	}
-
-//	public RequestPacketHeader(String header) {
-//		super(header);
-//		String[] split = header.split(" ");
-//		
-//		filename = split[1];
-//		Path path = Config.getPath();
-//		File f = new File(path + "\\" + filename);
-//		try {
-//			filesize = Files.size(f.toPath());
-//		} catch (IOException e1) {
-//			System.err.println("Failed to parse filesize.");
-//		}
-//	}
-//	
-//	public Message interpret(DropOSProtocol protocol) throws IOException {
-//		File file = receiveFile(protocol);
-//		System.out.println("Received requested file.");
-//		return new FileAndMessage("REQUEST " + filename, file);
-//	}
-
 }
