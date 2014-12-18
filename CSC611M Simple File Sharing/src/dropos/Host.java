e3d673e5993ee9f882104f72f7a1e0214ae4b750
Darren Karl A. Sapalo <darren.sapalo@gmail.com>
2014-12-18 22:50:55 +0800

    Added Factory pattern for creating a DropOSProtocol for Host class.

--- CSC611M Simple File Sharing/src/dropos/Host.java
+++ CSC611M Simple File Sharing/src/dropos/Host.java
@@ -1,8 +1,14 @@
 package dropos;
 
+import java.net.Socket;
+
+import message.DropOSProtocol;
+
 public class Host {
-	String ipAddress;
-	int port;
+	private DropOSProtocol protocol;
+	private String ipAddress;
+	private int port;
+	
 	
 	public Host(String ipAddress, int port) {
 		this.ipAddress = ipAddress;
@@ -21,4 +27,21 @@ public class Host {
 	public int getPort() {
 		return port;
 	}
+
+	public DropOSProtocol getProtocol() {
+		return protocol;
+	}
+	
+	public DropOSProtocol createProtocol(){
+		DropOSProtocol protocol = null;
+		try {
+			Socket s = new Socket(ipAddress, port);
+			protocol = new DropOSProtocol(s);
+		}catch(Exception e){
+			System.err.println("Could not create a connection with the host " + toString());
+			e.printStackTrace();
+		}
+		return protocol;
+	}
+
 }
