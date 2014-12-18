d6c73d45c8ed9ba9ad3bb194c3bdfc13da494872
addicteduser <tintin.kalaw@gmail.com>
2014-12-18 21:02:40 +0800

    Beginning with duplicate packet header.

--- CSC611M Simple File Sharing/src/message/DuplicatePacketHeader.java
+++ CSC611M Simple File Sharing/src/message/DuplicatePacketHeader.java
@@ -1,16 +1,44 @@
 package message;
 
-public class DuplicatePacketHeader extends PacketHeader{
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.function.LongPredicate;
+
+import dropos.Config;
+
+public class DuplicatePacketHeader extends FilePacketHeader{
+	
+	String duplicateheader;
+	String command;
+	ArrayList<String> ipAddresses = new ArrayList<String>();
 
 	public DuplicatePacketHeader(String header) {
 		super(header);
-		String[] split = header.split(":");
-		// get the ip addresses
-		// DUPLICATE:ipaddress1:ipaddress2:ipaddressN
-	}
-
-	public Message interpret(DropOSProtocol protocol) {
+		String[] split = header.split("\n");
+		this.header = split[1];
 		
-		return new Message(header);
+		duplicateheader = split[0];
+		split = duplicateheader.split(":");
+		for(int i = 0; i < split.length; i++) {
+			if (i == 0)
+				command = split[i];
+			else
+				ipAddresses.add(split[i]);
+		}
+		
+		filesize = Long.parseLong(this.header.split(":")[1]);
+		filename = this.header.split(":")[2];
+	}
+	
+	@Override
+	protected String filePath() {
+		return Config.getPath() + "\\" + filename;
+	}
+	
+	public void duplicateRedundancy(DropOSProtocol protocol) {
+		for(String ip : ipAddresses) {
+			PacketHeader updatepacket = PacketHeader.create("UPDATE:"+filesize+":"+filename);
+			// protocol.sendFile(updatepacket, f);
+		}
 	}
 }
