28324394aed9186cae43fa659629a0a43c659949
Darren Karl A. Sapalo <darren.sapalo@gmail.com>
2014-12-19 22:24:38 +0800

    Created basic code for UpdatePacketHeader class and fixed duplicateRedundancy method of DuplicatePacketHeader class.

--- CSC611M Simple File Sharing/src/message/packet/UpdatePacketHeader.java
+++ CSC611M Simple File Sharing/src/message/packet/UpdatePacketHeader.java
@@ -1,9 +1,23 @@
 package message.packet;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class UpdatePacketHeader extends FilePacketHeader {
 
-	public UpdatePacketHeader(int port, String header) {
-		super(port, header);
+	public UpdatePacketHeader(int port, String filename) {
+		super(port, "UPDATE");
+		this.filename = filename;
+		
+		String path = filePath();
+		Path p = Paths.get(path);
+		this.filesize = p.toFile().length();
+		
+		this.header += ":" + filesize;
+		
+		this.header += ":" + filename;
 	}
+	
+	
 
 }
